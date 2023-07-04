5e3dc327dfa7eeb753f31eec0d61270a69b55740
iremkumlu <61654683+iremkumlu@users.noreply.github.com>
2023-07-04 14:02:34 +0300

    my first react project
    React ile mini web site çalışması

---
+++ myfirstwebsite/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <h1>LOGO</h1>
+        <ul>
+          <li>Main page </li>
+          <li>Project</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <section className="backimage"></section>
+    </>
+  );
+}
+
+export default App;
